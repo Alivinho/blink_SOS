4cda554d07649cea96b3ca89e2e98d96dac58237
Alisson Livio <livilia74629@gmail.com>
2025-01-12 10:39:29 -0300

    Código do sinal de SOS

--- blink_SOS/blink_SOS.c
+++ blink_SOS/blink_SOS.c
@@ -2,12 +2,53 @@
 #include "pico/stdlib.h"
 
 
-int main()
-{
+int main(){
     stdio_init_all();
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    uint led_pin = 12;
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+
+    while (true){
+        for (int i = 0; i < 3; i++) {
+            blinkPonto(led_pin);
+        }
+
+        // Intervalo entre "S" e "O"
+        sleep_ms(250);
+
+        // Envia o "O" (3 traços)
+        for (int i = 0; i < 3; i++) {
+            blinkTraco(led_pin);
+        }
+
+        // Intervalo entre "O" e o segundo "S"
+        sleep_ms(250);
+
+        // Envia o segundo "S" (3 pontos)
+        for (int i = 0; i < 3; i++) {
+            blinkPonto(led_pin);
+        }
+
+        sleep_ms(3000);
+
+       
     }
+
+    return 0; 
+   
+}
+
+void blinkPonto(uint pino){
+    gpio_put(pino, 1);  
+    sleep_ms(200);     
+    gpio_put(pino, 0);  
+    sleep_ms(125); 
+}
+
+void blinkTraco(uint pino){
+    gpio_put(pino, 1);  
+    sleep_ms(800);     
+    gpio_put(pino, 0);  
+    sleep_ms(125); 
 }
